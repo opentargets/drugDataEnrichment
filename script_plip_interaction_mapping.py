a5d37e700b50b609b7dd3c786cfbe388e8fb3c97
Marine Girardey <marineland24@gmail.com>
2022-04-17 17:51:55 +0100

    plip script added

--- script_plip_interaction_mapping.py
+++ script_plip_interaction_mapping.py
@@ -4,18 +4,8 @@ from json import JSONDecodeError
 import requests
 from functools import reduce
 import pandas as pd
-from pyspark.sql.functions import (
-    col, udf, struct, lit, split, expr, collect_set, struct, 
-    regexp_replace, min as pyspark_min, explode, when,
-    array_contains, count, first, element_at, size, sum as pyspark_sum, array
-)
-from pyspark.sql.types import (
-    FloatType, ArrayType, StructType, StructField, BooleanType, StringType, IntegerType
-)
-from pyspark.sql import SparkSession
-from pyspark.conf import SparkConf
-from collections import defaultdict
-from pyspark.context import SparkContext
+import psutil
+
 from plip.basic import config
 
 from plip.structure.preparation import PDBComplex, PLInteraction
@@ -23,21 +13,18 @@ from plip.exchange.report import BindingSiteReport
 from plip.basic import config
 
 import dask.dataframe as dd
-
+from pandarallel import pandarallel
 from itertools import chain
 
 import logging
-
 import argparse
-
 import sys
+import time
+
+from pyspark.sql import SparkSession
 
 
 def main():
-
-    logging.info('Program begin.')
-
-    config.DNARECEPTOR = True
 
     # establish spark connection
     spark = (
@@ -46,29 +33,32 @@ def main():
         .getOrCreate()
     )
 
+    logging.info('Program begin.')
+
+    config.DNARECEPTOR = True
+
     logging.info('Loading input.')
 
-    # Dataset witht all the details, produced earlier:
+    # # Dataset witht all the details, produced earlier:
+    # input_dataset = (
+    #     pd.read_json(args.input_file, lines=True)
+    #     )
+    
     input_dataset = (
-        spark.read.csv(args.input_file, sep=",", header=True)
-        .groupBy("pdbStructureId")
-        .agg(collect_set(col("pdbCompoundId")).alias("pdbCompoundId"))
+        spark.read.json(args.input_file)
+        .select("pdbStructureId", "chains", "compoundIds")
         .toPandas()
     )
 
-    logging.info('Start compute PLIP interactions with Swifter parallelisation.')
+    print(input_dataset)
 
-    ddf = dd.from_pandas(input_dataset, npartitions=args.nb_partitions)
+    pandarallel.initialize(
+        nb_workers=psutil.cpu_count(),
+        progress_bar=True,
+    )
 
-    input_dataset = (
-        ddf
-        .assign(
-            new_col = ddf.map_partitions(
-                lambda df: df.apply(lambda row: characerize_complex(row), axis=1), meta=(None, 'f8')
-            )
-    #        .map_partitions(lambda df: df.apply(run_plip, axis=1), meta=(None, 'f8'))
-        )
-        .compute(scheduler='processes')
+    input_dataset['new_col'] = input_dataset.parallel_apply(
+        characerize_complex, axis=1
     )
 
     logging.info('PLIP interaction computations finished.')
@@ -87,30 +77,39 @@ def main():
 
 
 class GetPDB:
-    
+
     PDB_URL = 'https://www.ebi.ac.uk/pdbe/entry-files/download/pdb{}.ent'
-    
+
     def __init__(self, data_folder: str) -> None:
         self.data_folder = data_folder
-        
-    
+
+
     def get_pdb(self, pdb_structure_id: str) -> str:
         """Reading file from a given loaction fetch and save if not found"""
+
+        logging.info(f'Start searching the file.')
+
         try:
             # Readind data from the given location:
             with open(f'{self.data_folder}/pdb{pdb_structure_id}.ent', 'rt') as f:
+                logging.info(f'try.')
                 data = f.read()
-    
+                logging.info(f'try ok.')
+
         except FileNotFoundError:
+            logging.info(f'except.')
             # Fetch data from the web
             data = self.fetch_pdb(pdb_structure_id)
-            
+
             # Save file
             with open(f'{self.data_folder}/pdb{pdb_structure_id}.ent', 'wt') as f:
                 f.write(data)
-    
+                logging.info(f'except ok.')
+
+        logging.info(f'File obtained.')
+
         return data
-    
+
 
     def fetch_pdb(self, pdb_structure_id: str)-> str:
         """This function fetches the pdb file from ePDB server as a string
@@ -121,9 +120,9 @@ class GetPDB:
             structure data in pdb format as string eg 'AIN:A:1202'
         """
         data = ""
-    
+
         headers={'Content-Type': 'text/plain'}
-    
+
         if not pdb_structure_id:
             return ''
 
@@ -133,9 +132,11 @@ class GetPDB:
                 pass
             else:
                 data = response.text
-    
+
         except:
             data = ''
+
+        logging.info(f'File obtained or exception while scrapping.')
 
         return data
 
@@ -143,7 +144,7 @@ class GetPDB:
 def parse_interaction(interaction: PLInteraction, compound_id:str, pdb_id:str) -> dict:
 
     interaction_type = interaction.__doc__.split('(')[0]
-    
+
     if interaction_type == 'waterbridge':
         return {}
 
@@ -158,42 +159,50 @@ def parse_interaction(interaction: PLInteraction, compound_id:str, pdb_id:str) -
     }
 
 
+
 def characerize_complex(row):
-    # Get pdb data:
-    pdb_id = row['pdbStructureId']
-    compounds = row['pdbCompoundId']
-    
+
+    start_time = time.time()
+
+    pdb_id = row[0]
+    compounds = row[2]
+
+    logging.info(f'Start characerize_complex: {pdb_id, compounds}')
+
     gpdb = GetPDB(data_folder=args.pdb_folder)
 
     pdb_data = gpdb.get_pdb(pdb_id)
+    result = []
 
     if pdb_data:
 
         # Load into plip:
         mol_complex = PDBComplex()
-        
+
         try:
             mol_complex.load_pdb(pdb_data, as_string=True)
 
         except:
             pass
-        
+
         if mol_complex.ligands:
-            
+
             # Filtering out only the relevant ligands:
             ligands_of_interest = [ligand for ligand in mol_complex.ligands if ligand.hetid in compounds]
-                
+
             # Characterizing relevant complex:
             [mol_complex.characterize_complex(ligand) for ligand in ligands_of_interest]
 
-            # Extract details from ligands:
-            return [parse_interaction(interaction, compound.split(':')[0], pdb_id) for compound, interaction_set in mol_complex.interaction_sets.items() for interaction in interaction_set.all_itypes]
+            logging.info(f'Sites computation finished.')
 
-        else:
-            return []
-    
-    else:
-        return []
+            # Extract details from ligands:
+            result = [parse_interaction(interaction, compound.split(':')[0], pdb_id) for compound, interaction_set in mol_complex.interaction_sets.items() for interaction in interaction_set.all_itypes]
+
+    logging.info(f'Done characerize_complex {pdb_id} with ligands {compounds}')
+    # Log the complex info data: PDB ID, list of ligands, size of data, processing time in seconds.
+    logging.info(f'COMPLEX_INFO\t{pdb_id}\t{compounds}\t{len(pdb_data)}\t{time.time()-start_time}')
+
+    return result
 
 
 if __name__ == '__main__':
@@ -222,6 +231,14 @@ if __name__ == '__main__':
                         type=str,
                         required=True)
 
+    parser.add_argument('-l',
+                        '--log_file',
+                        help='File to save the logs to.',
+                        default=None,
+                        metavar='log_file',
+                        type=str,
+                        required=True)
+
     parser.add_argument('-f',
                         '--pdb_folder',
                         help='Path to the pdb folder with pdb files downloaded from pdbe website and used by PLIP to compute interactions.',
@@ -229,23 +246,10 @@ if __name__ == '__main__':
                         metavar='pdb_folder_path',
                         type=str,
                         required=True)
-    
-    parser.add_argument('-p',
-                        '--nb_partitions',
-                        help='Number of Dask partitions (I build 30 partitions with 8 cores).',
-                        default=None,
-                        metavar='nb_dask_partitions',
-                        type=int,
-                        required=True)
 
     args = parser.parse_args()
 
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
-    )
-    logging.StreamHandler(sys.stderr)
+    logging.basicConfig(filename=args.log_file, level=logging.INFO, force=True)
 
     logging.info(program_description)
 
